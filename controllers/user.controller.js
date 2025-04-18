067a2e34569680fd2ad70c0bd0af1dc389d0ca67
Harsh093-k <harshkashyap701153@gmail.com>
2025-04-18 11:51:37 +0530

    done

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -124,7 +124,7 @@ export const login = async (req, res) => {
         }
         return res.cookie('token', token, {
             httpOnly: true, secure: true,
-            sameSite: 'none', maxAge: 1 * 24 * 60 * 60 * 1000
+            sameSite: 'static', maxAge: 1 * 24 * 60 * 60 * 1000
         }).json({
             message: `Welcome back ${user.username}`,
             success: true,
