f9ffaf02c743ae41c5abe31224a364c0df28711a
Harsh093-k <harshkashyap701153@gmail.com>
2025-04-18 12:27:37 +0530

    done

--- utils/datauri.js
+++ utils/datauri.js
@@ -1,14 +1,4 @@
-// import DataUriParser from "datauri/parser.js";
-// import path from "path";
 
-// const getDataUri = (file) => {
-//   const parser = new DataUriParser();
-
-//   const extName = path.extname(file.originalname).toString();
-//   return parser.format(extName, file.buffer);
-// };
-
-// export default getDataUri;
 import DataUriParser from "datauri/parser.js";
 import path from "path";
 
@@ -16,7 +6,7 @@ const parser = new DataUriParser();
 
 const getDataUri = (file) => {
   const extName = path.extname(file.originalname).toLowerCase(); 
-  const validExtensions = ['.jpg', '.jpeg', '.png', '.webp', '.gif'];
+  const validExtensions = ['.jpg', '.jpeg', '.png', '.webp', '.gif','.mp4'];
 
   if (!validExtensions.includes(extName)) {
     throw new Error(`Unsupported file type: ${extName}`);
