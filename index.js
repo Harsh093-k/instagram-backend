1874e27048dac16c0339c9d017d68fdb728ed6ec
Harsh093-k <harshkashyap701153@gmail.com>
2025-04-19 13:50:10 +0530

    done

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-    origin:'https://instagram-frontend-rp7y.vercel.app',
+    // origin:'https://instagram-frontend-rp7y.vercel.app',
+    origin:' http://localhost:5173',
     credentials: true
 }
 app.use(cors(corsOptions));
