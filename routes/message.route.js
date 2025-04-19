6119bb8f21cfc1a051ea017797a1a98026008ef6
Harsh093-k <harshkashyap701153@gmail.com>
2025-04-19 12:16:55 +0530

    done

--- routes/message.route.js
+++ routes/message.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
-import { getMessage, sendMessage } from "../controllers/message.controller.js";
+import { deleteMessage, getMessage, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
